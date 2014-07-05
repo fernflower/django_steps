1c5fec4917d5ee35bbebd13d8660440377338116
Ina <inavasilevskaya@gmail.com>
2014-07-05 22:55:39 +0000

    contacts tests (server validation)

--- mysite/contacts/tests.py
+++ mysite/contacts/tests.py
@@ -9,11 +9,33 @@ from contacts.models import Message
 
 # Create your tests here.
 class MessagePostTest(TestCase):
-    def test_send_message(self):
-        # in order to get captcha
+    def get_captcha_hash(self):
         html = self.client.get(reverse("contacts:show_form"))
         p = PyQuery(html.content)
-        captcha0 = p('#id_captcha_0').attr('value')
+        return p('#id_captcha_0').attr('value')
+
+    def test_message_validation(self):
+        # bad email and no captcha -> server-side validation
+        msg = {"name": "ina", "email": "bad email",
+               "phone": 123, "text": "a test message"}
+        response = self.client.post(reverse("contacts:show_form"), msg)
+        p = PyQuery(response.content)
+        self.assertEqual(len(p('.form-group.has-error')), 2)
+        self.assertContains(response, "Enter a valid email address.")
+        self.assertContains(response, "This field is required.")
+        # now bad captcha only
+        msg = {"name": "ina", "email": "ok@email.ru",
+               "phone": 123, "text": "a test message",
+               "captcha_0": self.get_captcha_hash(),
+               "captcha_1": "BAD GUESS"}
+        response = self.client.post(reverse("contacts:show_form"), msg)
+        p = PyQuery(response.content)
+        self.assertEqual(len(p('.form-group.has-error')), 1)
+        self.assertContains(response, "Invalid CAPTCHA")
+        # TODO should write tests for client side validation as well
+
+    def test_send_message(self):
+        captcha0 = self.get_captcha_hash()
         captcha1 = CaptchaStore.objects.filter(hashkey=captcha0)
         self.assertEqual(len(captcha1), 1)
         captcha1 = captcha1[0].challenge
@@ -22,7 +44,7 @@ class MessagePostTest(TestCase):
                "captcha_0": captcha0,
                "captcha_1": captcha1}
         sent_text = \
-                "Спасибо! Мы получили Ваше сообщение и в скором времени свяжемся с Вами! =)"
+            "Спасибо! Мы получили Ваше сообщение и в скором времени свяжемся с Вами! =)"
         # test that after post message is shown in database
         db_msg = Message.objects.filter(sender_email=msg["email"])
         self.assertTrue(len(db_msg) == 0)
@@ -40,9 +62,8 @@ class MessagePostTest(TestCase):
     def test_no_message_edit_in_admin(self):
 
         def get_admin_url(msg):
-            return reverse("admin:%s_%s_change" % (msg._meta.app_label,
-                                                   msg._meta.module_name),
-                            args=(msg.id,))
+            return reverse("admin:%s_%s_change" %
+                           (msg._meta.app_label, msg._meta.module_name), args=(msg.id,))
 
         User.objects.create_superuser('admin', 'admin@test.ru', 'password')
         msg = Message(sender_email="ina@test.ru", sender_name="ina",
