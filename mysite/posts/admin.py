d0e994c7b17509702a0501624ffb774b5b6f5bc6
Ina <inavasilevskaya@gmail.com>
2014-07-11 23:05:49 +0400

    a clumsy way to delete all items in admin

--- mysite/posts/admin.py
+++ mysite/posts/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin import SummernoteModelAdmin, AttachmentAdmin
+from django_summernote.models import Attachment
 from posts.models import Post
 
 
-class PostAdmin(SummernoteModelAdmin):
-    fieldsets = [ (None, {'fields': ['title', 'text']}),
-                  ('Date information', {'fields': ['pub_date']}), ]
+class DeleteAllAdmin(admin.ModelAdmin):
+    actions = ['remove_all_items']
+
+    def remove_all_items(self, request, queryset):
+        self.model.objects.all().delete()
+    remove_all_items.short_description = "Delete all items"
+
+
+class PostAdmin(SummernoteModelAdmin, DeleteAllAdmin):
+    fieldsets = [(None, {'fields': ['title', 'text']}),
+                 ('Date information', {'fields': ['pub_date']}), ]
+    list_display = ['title', 'pub_date']
+
+
+class MyAttachmentAdmin(AttachmentAdmin, DeleteAllAdmin):
+    pass
+
 
 admin.site.register(Post, PostAdmin)
+admin.site.unregister(Attachment)
+admin.site.register(Attachment, MyAttachmentAdmin)
