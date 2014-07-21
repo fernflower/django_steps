fed1214459718f9343093e661a7cc76bd5054543
Ina Vasilevskaya <i-vasilevskaja@igrade.ru>
2014-07-21 12:16:31 +0400

    text cut on front page + link to more

--- mysite/posts/templatetags/posts_tags.py
+++ mysite/posts/templatetags/posts_tags.py
@@ -1,31 +1,41 @@
 from pyquery import PyQuery
 from django import template
+from django.core.urlresolvers import reverse
 
 
 def do_cut_images(parser, token):
     try:
-        tag_name, html = token.split_contents()
+        tag_name, html, post_id = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError(
             "{} tag requires a single argument".format(token.contents.split()[0]))
-    return CurrentHtmlNode(html)
+    return CurrentHtmlNode(html, post_id)
 
 
 class CurrentHtmlNode(template.Node):
-    def __init__(self, html):
+    def __init__(self, html, post_id):
         self.html = html
+        self.post_id = post_id
 
     def render(self, context):
         result = super(CurrentHtmlNode, self).render(context)
         t = template.Template(self.html)
+        post_id = template.Template(self.post_id).render(context)
         rendered_html = t.render(context)
         pq = PyQuery(rendered_html)
-        occurences = pq('img')
-        if len(occurences) > 2:
-            for img in occurences[2:]:
-                img.clear()
-        result_html = pq.html()
-        return result_html
+        img_count = 0
+        first_clear = True
+        for tag in pq.children():
+            if tag.tag == 'img':
+                img_count += 1
+            if img_count > 2:
+                tag.clear()
+                # add '...' to show that more data is hidden under cut
+                if first_clear:
+                    first_clear = False
+                    pq.append("<a href='{}'>(Больше фотографий)</a>".format(
+                        reverse('posts:detail', kwargs={'pk': post_id})))
+        return pq.html()
 
 
 register = template.Library()
