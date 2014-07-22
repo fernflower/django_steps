b7607d2f51db2bd4a31e6c83d92a48b5bd29f649
Ina Vasilevskaya <i-vasilevskaja@igrade.ru>
2014-07-22 17:27:26 +0400

    post date and time obligatory no more

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -6,10 +6,15 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=400)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', blank=True, null=True)
 
     def __str__(self):
         return self.title if self.title != '' else self.text[:50]
+
+    def save(self, *args, **kwargs):
+        if not self.pub_date:
+            self.pub_date = datetime.datetime.today()
+        return super(Post, self).save(*args, **kwargs)
 
     def is_published_recently(self):
         now = timezone.now()
