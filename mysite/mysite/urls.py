26005b594c2ee6b96031dd16a912829c5de133f4
Inessa Vasilevskaya <ivasilevskaya@mirantis.com>
2017-08-11 16:22:02 +0300

    django: posts app served at root

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,11 +1,9 @@
 import django.conf.urls as urls
 from django.contrib import admin
-from django.views.generic import TemplateView
-import settings
 
 admin.autodiscover()
 
 urlpatterns = urls.patterns(
     '',
-    urls.url(r'^posts/', urls.include('posts.urls', namespace="posts")),
+    urls.url(r'^', urls.include('posts.urls', namespace="posts")),
     urls.url(r'^admin/', urls.include(admin.site.urls)),)
