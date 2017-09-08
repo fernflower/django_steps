a2072006a0a623a164d2b9b717e177fa95e7a863
Inessa Vasilevskaya <ivasilevskaya@mirantis.com>
2017-09-09 00:46:30 +0300

    html: meta tags added to inserted videos
    videos order changed a bit

--- mysite/common_static/freelancer/js/insert_videos.js
+++ mysite/common_static/freelancer/js/insert_videos.js
@@ -2,6 +2,22 @@ var videos_block = 1;
 var videos_num = 9;
 var show_more_button_enabled = true;
 
+function create_meta_elem(name, content, append_to) {
+    meta = document.createElement("meta");
+    meta.setAttribute("itemprop", name);
+    meta.setAttribute("content", content);
+    append_to.appendChild(meta);
+    return meta;
+}
+
+function create_link_elem(name, href, append_to) {
+    link = document.createElement("link");
+    link.setAttribute("itemprop", name);
+    link.setAttribute("href", href);
+    append_to.appendChild(link);
+    return link;
+}
+
 function do_insert(data) {
     $.each(data.videos,
            function() {
@@ -17,6 +33,14 @@ function do_insert(data) {
                iframe.setAttribute("class", "embed-responsive-item");
                iframe.setAttribute("allowfullscreen", "");
                iframe.setAttribute("src", this[1]);
+               create_meta_elem("isFamilyFriendly", "true", a_div);
+               create_meta_elem("uploadDate", this[3], a_div);
+               create_meta_elem("name", this[2], a_div);
+               create_meta_elem("description", this[2], a_div);
+               create_meta_elem("thumbnail", "https://img.youtube.com/vi/" + this[0] + "/0.jpg", a_div);
+               create_meta_elem("duration", this[4], a_div);
+               create_link_elem("url", this[1], a_div);
+               create_link_elem("thumbnailUrl", "https://img.youtube.com/vi/" + this[0] + "/0.jpg", a_div);
                div.appendChild(a);
                a.appendChild(a_div);
                a_div.appendChild(iframe);
